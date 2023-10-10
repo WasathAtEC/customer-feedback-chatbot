c1d69299e923e962bf5370b523b8afe9653ac0ca
Isuru Sasanga Kolombage <isurusasanga1999@gmail.com>
2023-10-10 21:50:48 +0530

    feat: initiate the structure

--- chatbot/src/app/components/chatBotPage.jsx
+++ chatbot/src/app/components/chatBotPage.jsx
@@ -5,6 +5,7 @@ const ChatBotPage = () => {
             <div className='bg-gradient-to-r from-[#3054D9] to-[#50ADF2] p-5 mb-2 rounded-t-xl'>
                 <h1 className="text-center text-white text-[32px] font-extrabold pb-1">Hi there! </h1>
             </div>
+
             <div>
                 <p>Hi, I'm DOC ChatBot</p>
             </div>
@@ -13,6 +14,26 @@ const ChatBotPage = () => {
             </div>
             <div>
                 <p>Please choose one of the topics. <br />What would you like to explore?</p>
+            </div>
+
+            <div>
+                <div>issue 01</div>
+                <div>issue 02</div>
+                <div>issue 03</div>
+                <div>issue 04</div>
+                <div>issue 05</div>
+            </div>
+
+            <div>
+                
+            </div>
+
+            <div>
+                <p>For complex issues,<br />please use our Support Form. <br /> We'll respond promptly.</p>
+            </div>
+
+            <div>
+                <p>support form</p>
             </div>
         </div>
     );
