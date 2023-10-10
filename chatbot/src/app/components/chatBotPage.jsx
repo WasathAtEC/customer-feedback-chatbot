7c12d3536fef272eba98e80a86957093b34efa6d
Isuru Sasanga Kolombage <isurusasanga1999@gmail.com>
2023-10-10 21:40:54 +0530

    feat: initiate chatbotpage

---
+++ chatbot/src/app/components/chatBotPage.jsx
@@ -0,0 +1,21 @@
+
+const ChatBotPage = () => {
+    return (
+        <div className="max-w-md mx-auto my-10 shadow-md rounded-xl bg-[#C7EFF8]">
+            <div className='bg-gradient-to-r from-[#3054D9] to-[#50ADF2] p-5 mb-2 rounded-t-xl'>
+                <h1 className="text-center text-white text-[32px] font-extrabold pb-1">Hi there! </h1>
+            </div>
+            <div>
+                <p>Hi, I'm DOC ChatBot</p>
+            </div>
+            <div>
+                <p>How can I help you today?</p>
+            </div>
+            <div>
+                <p>Please choose one of the topics. <br />What would you like to explore?</p>
+            </div>
+        </div>
+    );
+};
+
+export default ChatBotPage;
--- chatbot/src/app/page.jsx
+++ chatbot/src/app/page.jsx
@@ -1,9 +1,12 @@
+import ChatBotPage from "./components/chatBotPage";
 import CustomerServiceForm from "./components/cutomerServiceForm";
 
 export default function Home() {
   return (
     <>
+    <ChatBotPage />
     <CustomerServiceForm />
+    
     </>
   )
 }
