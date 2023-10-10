ccb687d68a3fb9c63141f35ccb6854f18f8e76dc
Isuru Sasanga Kolombage <isurusasanga1999@gmail.com>
2023-10-10 23:53:21 +0530

    feat: add styles to msg

--- chatbot/src/app/components/chatBotPage.jsx
+++ chatbot/src/app/components/chatBotPage.jsx
@@ -6,20 +6,20 @@ const ChatBotPage = () => {
                 <h1 className="text-center text-white text-[32px] font-extrabold pb-1">Hi there! </h1>
             </div>
 
-            <div className="px-6 pt-2 pb-8">
-                <div className="w-auto h-9 px-[21px] py-[9px] bg-gray-400 bg-opacity-50 rounded-[20px] shadow border border-stone-600 border-opacity-20 justify-center items-center gap-2.5 inline-flex mb-2">
-                    <p className="text-stone-950 text-opacity-70 text-[15px] font-semibold ">Hi, I'm DOC ChatBot</p>
+            <div className="px-10 pt-2 pb-8">
+                <div className="w-auto h-10 px-[21px] py-[9px] bg-gray-400 bg-opacity-50 rounded-[20px] shadow border border-stone-600 border-opacity-20 justify-center items-center gap-2.5 inline-flex mb-3">
+                    <p className="text-stone-950 text-opacity-70 text-[16px] font-semibold ">Hi, I'm DOC ChatBot</p>
                 </div>
 
-                <div className="w-auto h-9 px-[21px] py-[9px] bg-gray-400 bg-opacity-50 rounded-[20px] shadow border border-stone-600 border-opacity-20 justify-center items-center gap-2.5 inline-flex mb-2">
-                    <p className="text-stone-950 text-opacity-70 text-[15px] font-semibold ">How can I help you today?</p>
+                <div className="w-auto h-10 px-[21px] py-[9px] bg-gray-400 bg-opacity-50 rounded-[20px] shadow border border-stone-600 border-opacity-20 justify-center items-center gap-2.5 inline-flex mb-3">
+                    <p className="text-stone-950 text-opacity-70 text-[16px] font-semibold ">How can I help you today?</p>
                 </div>
 
-                <div className="w-auto h-[68px] px-[21px] py-[9px] bg-gray-400 bg-opacity-50 rounded-[20px] shadow border border-stone-600 border-opacity-20 justify-center items-center gap-2.5 inline-flex mb-4">
-                    <p className="text-stone-950 text-opacity-70 text-[15px] font-semibold leading-[25px]">Please choose one of the topics. <br />What would you like to explore?</p>
+                <div className="w-auto h-[70px] px-[26px] py-[9px] bg-gray-400 bg-opacity-50 rounded-[20px] shadow border border-stone-600 border-opacity-20 justify-center items-center gap-2.5 inline-flex mb-5">
+                    <p className="text-stone-950 text-opacity-70 text-[16px] font-semibold leading-[28px]">Please choose one of the topics. <br />What would you like to explore?</p>
                 </div>
 
-                <div className="w-auto h-auto gap-[18px] flex flex-wrap px-2">
+                <div className="w-auto h-auto gap-[18px] flex flex-wrap px-2 my-3">
                     <div className="outline-dashed outline-2 outline-offset-0 rounded-lg bg-[#9ACAF6] opacity-58 text-blue-600 text-opacity-90 text-md font-semibold font-['Inter'] leading-[25px] px-4 py-1 mb-2 mx-1">issue 01</div>
                     <div  className="outline-dashed outline-2 outline-offset-0 rounded-lg bg-[#9ACAF6] opacity-58 text-blue-600 text-opacity-90 text-md font-semibold font-['Inter'] leading-[25px] px-4 py-1 mb-2 mx-1">issue 02</div>
                     <div  className="outline-dashed outline-2 outline-offset-0 rounded-lg bg-[#9ACAF6] opacity-58 text-blue-600 text-opacity-90 text-md font-semibold font-['Inter'] leading-[25px] px-4 py-1 mb-2 mx-1">issue 03</div>
@@ -31,8 +31,8 @@ const ChatBotPage = () => {
                     
                 </div>
 
-                <div>
-                    <p>For complex issues,<br />please use our Support Form. <br /> We'll respond promptly.</p>
+                <div className="w-auto h-[96px] px-[21px] py-[9px] bg-gray-400 bg-opacity-50 rounded-[20px] shadow border border-stone-600 border-opacity-20 justify-center items-center gap-2.5 inline-flex my-4">
+                    <p className="text-stone-950 text-opacity-70 text-[16px] font-semibold leading-[25px]">For complex issues,<br />please use our Support Form. <br /> We'll respond promptly.</p>
                 </div>
 
                 <div>
