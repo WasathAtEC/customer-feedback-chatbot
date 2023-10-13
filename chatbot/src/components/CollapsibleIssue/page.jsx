08b42ee9bd8178b16407f72681175082e7cc76f9
Isuru Sasanga Kolombage <isurusasanga1999@gmail.com>
2023-10-13 23:11:30 +0530

    feat: style the issue buttons

--- chatbot/src/components/CollapsibleIssue/page.jsx
+++ chatbot/src/components/CollapsibleIssue/page.jsx
@@ -1,20 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const CollapsibleIssue = ({ issue, details, isOpen, onClick }) => {
-  
   return (
     <div className="mb-2">
       <button
         onClick={onClick}
-        className="text-blue-600 cursor-pointer"
+        className="outline-dashed outline-2 outline-offset-0 rounded-lg bg-[#9ACAF6] opacity-58 text-blue-600 text-opacity-90 text-md font-semibold font-['Inter'] leading-[25px] px-4 py-1 mb-2 mx-1"
       >
         {issue}
       </button>
-      {isOpen && (
-        <div className="ml-4 text-gray-600">
-          {details}
-        </div>
-      )}
+      {isOpen && <div className="ml-4 text-gray-600">{details}</div>}
     </div>
   );
 };
